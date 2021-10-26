f359c8821701332a11befce8130bb37a9be9b9d9
gumo <hu.yong@intellif.com>
2021-10-26 19:46:56 +0800

    python 图片解析

---
+++ main/test/test.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# test.py
+
+import os
+
+print(os.path.abspath('.'))
+print(__file__)
--- main/test/定时任务threading.py
+++ main/test/定时任务threading.py
@@ -9,6 +9,7 @@ def do_job():
     timer = threading.Timer(86400, do_job)  # 86400秒就是一天
     timer.start()
 
+
 # 计算当前时间到指定时间点的描述差
 def get_interval_secs(target_time):
     today = (datetime.date.today()).strftime('%Y%m%d')
@@ -17,8 +18,11 @@ def get_interval_secs(target_time):
     now = datetime.datetime.now()
     interval = today_time_date - now
     secs = interval.total_seconds()
-    if (secs > 0): return secs
-    else: return secs + 86400
+    if secs > 0:
+        return secs
+    else:
+        return secs + 86400
+
 
 timer = threading.Timer(get_interval_secs("21:00:00"), do_report)
 timer.start()
